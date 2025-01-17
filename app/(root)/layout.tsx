ad48fbc2c26d932c777706a3b7e740358714a475
Chaitanya <chaitu.vsh@gmail.com>
2025-01-16 23:52:17 -0800

    [AUTH] Github auth

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,12 @@
+import Navbar from "../components/Navbar";
+
+const Layout = ({children}: {children: React.ReactNode}) => {
+  return (
+    <main className="font-sans">
+        <Navbar />
+        {children}
+    </main>
+  )
+}
+
+export default Layout;
