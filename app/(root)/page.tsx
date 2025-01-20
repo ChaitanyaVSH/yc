3841cb4d5c8afbe5175db9432d78294aa29e8486
Chaitanya <chaitu.vsh@gmail.com>
2025-01-19 18:32:24 -0800

    [HOME] Adding home page section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,13 @@
 const Home = () => {
   return (
-    <h1 className="heading">Home</h1>
+    <>
+    <section className="pink_container">
+      <h1 className="heading">Pitch Your Startup <br /> Connect With Entrepreneurs</h1>
+      <p className="sub-heading !max-w-3xl">
+        Submit Ideas, Vote on Pitches, and Get noticed in Virtual Competitions.
+      </p>
+    </section>
+    </>
   )
 }
 
