264b744e54e03078f1f154e1a897563c7f373beb
Chaitanya <chaitu.vsh@gmail.com>
2025-01-24 23:08:17 -0800

    ** [CRITICAL][SANITY] Enabling live filtering capacity

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -1,13 +1,18 @@
 import { defineQuery } from "next-sanity";
 
 export const STARTUPS_QUERY = defineQuery(
-    `*[_type=="startup"] | order(_createdAt desc) {
+    `*[_type=="startup" && 
+    !defined($search) || 
+    title match $search || 
+    category match $search || 
+    author->name match $search] | order(_createdAt desc) {
   title,
   slug,
   views,
   category,
   _createdAt,
   image,
+  _id,
   description,
   author -> {
     _id,
