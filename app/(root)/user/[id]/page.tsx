3563005c6a1f5abc5d614a4f892a9979c1fb039c
Chaitanya <chaitu.vsh@gmail.com>
2025-02-08 16:35:17 -0800

    Basic structure for User details page

--- app/(root)/user/[id]/page.tsx
+++ app/(root)/user/[id]/page.tsx
@@ -1,21 +1,53 @@
 import { auth } from "@/auth";
 import { client } from "@/sanity/lib/client";
 import { AUTHOR_BY_GITHUB_ID_QUERY } from "@/sanity/lib/queries";
+import Image from "next/image";
 import { notFound } from "next/navigation";
+import { Suspense } from "react";
+
+export const experimental_ppr = true;
 
 const page = async ({params}: {params: Promise<{id: string}>}) => {
 
   var session = await auth();
   var id = (await params).id;
 
-  console.log("id", id);
-  const user = await client.fetch(AUTHOR_BY_GITHUB_ID_QUERY, {id: Number(id)})
-  console.log("USER", user);
-  // if(!user) return notFound();
+  var user = await client.fetch(AUTHOR_BY_GITHUB_ID_QUERY, {id: Number(id)})
+  if(!user) return notFound();
 
 
   return (
-    <div>User details: {id}</div>
+    <>
+      <section className="profile_container">
+        <div className="profile_card">
+          <div className="profile_title">
+            <h3 className="text-24-black uppercase text-center">
+              {user.name}
+            </h3>
+          </div>
+
+          <Image
+            src={user.image}
+            alt={user.name}
+            width={220}
+            height={220}
+            className="profile_image"
+          />
+          <p className="text-30-extrabold mt-7 text-center">@{user?.username}</p>
+          <p className="mt-1 text-center text-14-normal">{user?.bio}</p>
+        </div>
+        <div className="flex-1 flex flex-col gap-5 lg:-mt-5">
+          <p className="text-30-bold">
+            {session.author_id === user?._id ? "Your Startups": "All Startups"}
+          </p>
+          <ul className="card_grid-sm">
+            <Suspense fallback={"Loading"}>
+              {/* {TODO: User startups} */}
+            </Suspense>
+          </ul>
+        </div>
+      </section>
+    </>
   )
 }
 
