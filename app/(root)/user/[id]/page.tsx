e8881d050c9ca273b6fafe1cab8a89ef260d77fb
Chaitanya <chaitu.vsh@gmail.com>
2025-02-08 16:15:58 -0800

    Fetching user from sanity

--- app/(root)/user/[id]/page.tsx
+++ app/(root)/user/[id]/page.tsx
@@ -1,6 +1,21 @@
-const page = () => {
+import { auth } from "@/auth";
+import { client } from "@/sanity/lib/client";
+import { AUTHOR_BY_GITHUB_ID_QUERY } from "@/sanity/lib/queries";
+import { notFound } from "next/navigation";
+
+const page = async ({params}: {params: Promise<{id: string}>}) => {
+
+  var session = await auth();
+  var id = (await params).id;
+
+  console.log("id", id);
+  const user = await client.fetch(AUTHOR_BY_GITHUB_ID_QUERY, {id: Number(id)})
+  console.log("USER", user);
+  // if(!user) return notFound();
+
+
   return (
-    <div>User details</div>
+    <div>User details: {id}</div>
   )
 }
 
